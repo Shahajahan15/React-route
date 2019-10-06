384444050621715a3efd38cd69f48d3fa67f4ea6
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-06 21:00:47 +0600

    Router Switch to Page not found

--- src/components/MyRoute.js
+++ src/components/MyRoute.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import page1 from './pages/page1';
 import page2 from './pages/page2';
 import page3 from './pages/page3';
 import page4 from './pages/page4';
 import Home from './pages/Home';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import notFound from './pages/notFound';
 
 class MyRoute extends Component {
     render() {
         return (
             <div className="d-inline">
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/page1" component={page1}></Route>
-                <Route exact path="/page2" component={page2}></Route>
-                <Route exact path="/page3" component={page3}></Route>
-                <Route exact path="/page4" component={page4}></Route>
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route path="/page1" component={page1}></Route>
+                    <Route path="/page2" component={page2}></Route>
+                    <Route path="/page3" component={page3}></Route>
+                    <Route path="/page4" component={page4}></Route>
+                    <Route component={notFound}></Route>
+                </Switch>
             </div>
         );
     }
---
+++ src/components/pages/notFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class notFound extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-danger"><strong>Oops!</strong> Page not found</h1>
+            </div>
+        );
+    }
+}
+
+export default notFound;
